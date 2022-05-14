ca58fe0b4d27ecdf220f538658458125c1844b88
alan <nguyentri.alan@gmail.com>
2022-05-14 20:40:41 +0700

    profile page

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -5,11 +5,13 @@ import Footer from "./Footer";
 import "./HomeView.css";
 import Menu from "../components/home/Menu";
 import ShowProduct from "../components/home/ShowProduct";
+import Profile from "../components/home/Profile";
 const HomeView = ({homeRoute}) => {
   let body = (
     <div>
       {homeRoute === 'dashboard' && <Dashboard />}
       {homeRoute === 'product' && <ShowProduct />}
+      {homeRoute === 'profile' && <Profile />}
     </div>
   )
   return (
